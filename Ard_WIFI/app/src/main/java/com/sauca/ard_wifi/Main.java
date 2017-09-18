1d07295abff11742001f1d00e14ab41348ffc493
carvbapt <carvbapt@gmail.com>
2017-09-18 14:59:17 +0100

    Update sinalizador

--- Ard_WIFI/app/src/main/java/com/sauca/ard_wifi/Main.java
+++ Ard_WIFI/app/src/main/java/com/sauca/ard_wifi/Main.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -14,6 +15,7 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
 
     Button btOn,btOff,btSair;
     TextView txt;
+    ImageView iv_wifi;
     WifiManager wf;
 
     @Override
@@ -29,6 +31,7 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
         btSair=(Button)findViewById(R.id.BT_Sair);
 
         txt=(TextView) findViewById(R.id.T_Tex);
+        iv_wifi=(ImageView)findViewById(R.id.wifi_image);
 
         btOn.setOnClickListener(this);
         btOff.setOnClickListener(this);
@@ -43,12 +46,14 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
             wf=(WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
             wf.setWifiEnabled(true);
             txt.setText("WIFI Turned ON");
+            iv_wifi.setImageResource(R.drawable.bt_green);
             //txt.setVisibility(View.VISIBLE);
             //Toast.makeText(this,"Hello ",Toast.LENGTH_SHORT).show();
         }else if(v==findViewById(R.id.BT_WFOff)){
             wf=(WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
             wf.setWifiEnabled(false);
             txt.setText("WIFI Turned OFF");
+            iv_wifi.setImageResource(R.drawable.bt_red);
             //txt.setVisibility(View.INVISIBLE);
             //Toast.makeText(this,"Goodbye ",Toast.LENGTH_SHORT).show();
         }else if(v==findViewById(R.id.BT_Sair)){
